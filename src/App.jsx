cbed5a5b67cbee5d8a7b1cdb93330d46a9cee650
freetimezp <kraftukrnet@ukr.net>
2023-03-28 20:10:53 +0300

    02. js-services-06-fly. Base style. File structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './components/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Search from './components/search/Search';
+import Support from './components/support/Support';
+import Info from './components/info/Info';
+import Lounge from './components/lounge/Lounge';
+import Travelers from './components/travelers/Travelers';
+import Subscribers from './components/subscribers/Subscribers';
+import Footer from './components/footer/Footer';
 
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Navbar />
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribers />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
